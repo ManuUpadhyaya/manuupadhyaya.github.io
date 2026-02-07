ee0cfb42702d18c7a8f60afccd5105b2420d1e18
ManuUpadhyaya <Manu.Upadhyaya.42@gmail.com>
2026-02-07 12:59:28 +0000

    Deploying to gh-pages from @ ManuUpadhyaya/manuupadhyaya.github.io@f3930f9a567f61089024f1ef8a5ed10fc03c58d4 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -23,29 +23,7 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/teaching/";
           },
-        },{id: "post-google-gemini-updates-flash-1-5-gemma-2-and-project-astra",
-      
-        title: 'Google Gemini updates: Flash 1.5, Gemma 2 and Project Astra <svg width="1.2rem" height="1.2rem" top=".5rem" viewBox="0 0 40 40" xmlns="http://www.w3.org/2000/svg"><path d="M17 13.5v6H5v-12h6m3-3h6v6m0-6-9 9" class="icon_svg-stroke" stroke="#999" stroke-width="1.5" fill="none" fill-rule="evenodd" stroke-linecap="round" stroke-linejoin="round"></path></svg>',
-      
-      description: "We’re sharing updates across our Gemini family of models and a glimpse of Project Astra, our vision for the future of AI assistants.",
-      section: "Posts",
-      handler: () => {
-        
-          window.open("https://blog.google/technology/ai/google-gemini-update-flash-ai-assistant-io-2024/", "_blank");
-        
-      },
-    },{id: "post-displaying-external-posts-on-your-al-folio-blog",
-      
-        title: 'Displaying External Posts on Your al-folio Blog <svg width="1.2rem" height="1.2rem" top=".5rem" viewBox="0 0 40 40" xmlns="http://www.w3.org/2000/svg"><path d="M17 13.5v6H5v-12h6m3-3h6v6m0-6-9 9" class="icon_svg-stroke" stroke="#999" stroke-width="1.5" fill="none" fill-rule="evenodd" stroke-linecap="round" stroke-linejoin="round"></path></svg>',
-      
-      description: "",
-      section: "Posts",
-      handler: () => {
-        
-          window.open("https://medium.com/@al-folio/displaying-external-posts-on-your-al-folio-blog-b60a1d241a0a?source=rss-17feae71c3c4------2", "_blank");
-        
-      },
-    },{id: "news-i-am-excited-to-announce-that-i-am-co-organizing-the-21st-conference-on-advances-in-continuous-optimization-europt-2024-set-to-take-place-in-lund-sweden-from-june-26-to-28-2024-for-more-details-visit-the-conference-website",
+        },{id: "news-i-am-excited-to-announce-that-i-am-co-organizing-the-21st-conference-on-advances-in-continuous-optimization-europt-2024-set-to-take-place-in-lund-sweden-from-june-26-to-28-2024-for-more-details-visit-the-conference-website",
           title: 'I am excited to announce that I am co-organizing the 21st conference on...',
           description: "",
           section: "News",},{id: "news-the-manuscript-titled-automated-tight-lyapunov-analysis-for-first-order-methods-has-been-accepted-for-publication-in-the-journal-mathematical-programming",
@@ -53,5 +31,14 @@ ninja.data = [{
           description: "",
           section: "News",},{id: "news-i-will-visit-the-sierra-team-at-inria-paris-from-september-2024-until-february-2025",
           title: 'I will visit the Sierra team at Inria Paris from September 2024 until...',
+          description: "",
+          section: "News",},{id: "news-i-successfully-defended-my-phd-thesis-many-thanks-to-my-opponent-bartolomeo-stellato-and-to-the-examining-committee-tony-stillfjord-ion-necoara-and-masoud-ahookhosh",
+          title: 'I successfully defended my PhD thesis. Many thanks to my opponent, Bartolomeo Stellato,...',
+          description: "",
+          section: "News",},{id: "news-the-manuscript-titled-the-chambolle-pock-method-converges-weakly-with-theta-amp-gt-1-2-and-tau-sigma-l-2-amp-lt-4-1-2-theta-has-been-accepted-for-publication-in-the-journal-optimization-letters",
+          title: 'The manuscript titled The Chambolle-Pock method converges weakly with $\theta &amp;amp;gt; 1/2\,\,$ and...',
+          description: "",
+          section: "News",},{id: "news-the-manuscript-titled-a-lyapunov-analysis-of-korpelevich-s-extragradient-method-with-fast-and-flexible-extensions-has-been-accepted-for-publication-in-the-journal-mathematical-programming",
+          title: 'The manuscript titled A Lyapunov analysis of Korpelevich’s extragradient method with fast and...',
           description: "",
           section: "News",},];
